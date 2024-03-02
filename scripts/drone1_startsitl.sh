3ce4f91252ab7974db3bc70d0de0d30765f047b5
fabeha-raheel <fabeha.raheel.aeelab@gmail.com>
2024-03-02 09:59:07 +0500

    created dronex_startsitl.sh files as well as outdoor_apm.launch file to launch Ardupilot SITL and MAVROS simulations

---
+++ scripts/drone1_startsitl.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cd ~/ardupilot/ArduCopter/ && sim_vehicle.py -v ArduCopter -f gazebo-iris1 --console -I0
---
+++ scripts/drone2_startsitl.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cd ~/ardupilot/ArduCopter/ && sim_vehicle.py -v ArduCopter -f gazebo-iris2 --console -I1
