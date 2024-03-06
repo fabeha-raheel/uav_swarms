57aa8f45a34eeafeaa73605f5f8381fb3162f2ba
fabeha-raheel <fabeha.raheel.aeelab@gmail.com>
2024-03-06 14:48:14 +0500

    modified SwarmLeader class to inherit from MAVROS_Drone class

--- scripts/SwarmLeader.py
+++ scripts/SwarmLeader.py
@@ -3,23 +3,16 @@
 import math
 import rospy
 
-from std_msgs.msg import Float64
-from sensor_msgs.msg import NavSatFix
-from nav_msgs.msg import Odometry
-from mavros_msgs.msg import GlobalPositionTarget
-from mavros_msgs.srv import CommandBool, CommandTOL, SetMode
-
-from Drone_Data import Drone_Data
+from MAVROS_Drone import MAVROS_Drone
 from SwarmFollower import SwarmFollower
 
 
-class SwarmLeader():
+class SwarmLeader(MAVROS_Drone):
     def __init__(self, name='drone1', n_followers=2) -> None:
 
-        self.leader_data = Drone_Data()
-        
-        self.leader_data.header.name = name
-        self.leader_data.header.id = 1
+        self.ns = name
+        self.data.header.name = name
+        self.data.header.id = 1
         
         self.n_followers = n_followers
         self.followers = []
@@ -31,126 +24,41 @@ class SwarmLeader():
             this_follower.data.header.id = i+2
             self.followers.append(this_follower)
         
-        self.GPS_Fix = None
-        
-        self.takeoff_altitude = 0
-        
         rospy.init_node('Swarm_Leader')
         
-        # Initialize Leader and Followers subscribers
+        self.initialize()
+        
+        # Wait for GPS Fix
+        self.wait_for_GPS_Fix()  
+        
+    def initialize(self):
+        # Initialize leader's ROS subscribers and publishers
         self.init_subscribers()
         self.init_publishers()
         
-        # Wait for GPS Fix
-        self.wait_for_GPS_Fix()            
-        
-    def init_subscribers(self):
-        self.leader_global_position_subscriber = rospy.Subscriber(self.leader_data.header.name + '/mavros/global_position/global',NavSatFix, self.leader_GPS_Subscriber_callback)
-        self.leader_local_position_subscriber = rospy.Subscriber(self.leader_data.header.name + '/mavros/global_position/local',Odometry, self.leader_LocalPos_Subscriber_callback)
-        self.leader_heading_subscriber = rospy.Subscriber(self.leader_data.header.name + '/mavros/global_position/compass_hdg',Float64, self.leader_Heading_Subscriber_callback)
-    
-        # initialize the subscribers for each of the followers
+        # Initialize follower's ROS subscribers and publishers
         for i in range(self.n_followers):
             self.followers[i].init_subscribers()
-            
-    def init_publishers(self):
-        self.leader_global_setpoint_publisher = rospy.Publisher(self.leader_data.header.name + '/mavros/setpoint_raw/global',GlobalPositionTarget, queue_size=1)
-        
-        # initialize the publishers for each of the followers
-        for i in range(self.n_followers):
             self.followers[i].init_publishers()
             
     def wait_for_GPS_Fix(self):
         while True:
-            if self.GPS_Fix == 0:
+            if self.data.global_position.gps_fix == 0:
                 print("Leader is ready!")
-                followers_ready = self.check_followers_GPS_Fix()
-                if followers_ready == self.n_followers:
+                if self.check_followers_GPS_Fix():
                     print("All followers are ready!")
                     break
         
     def check_followers_GPS_Fix(self):
         followers_ready = 0    
         for follower in self.followers:
-            if follower.GPS_Fix == 0:
+            if follower.data.global_position.gps_fix == 0:
                 followers_ready += 1
         
-        return followers_ready
-    
-    def arm(self):
-        rospy.wait_for_service(self.leader_data.header.name + '/mavros/cmd/arming', timeout=3)
-        try:
-            armService = rospy.ServiceProxy(self.leader_data.header.name + '/mavros/cmd/arming', CommandBool)
-            armResponse = armService(True)
-            rospy.loginfo(armResponse)
-        except rospy.ServiceException as e:
-            print("Service call failed: %s" %e)
-            
-        return armResponse
-
-    def disarm(self):
-        rospy.wait_for_service(self.leader_data.header.name + '/mavros/cmd/arming', timeout=3)
-        try:
-            armService = rospy.ServiceProxy(self.leader_data.header.name + '/mavros/cmd/arming', CommandBool)
-            armResponse = armService(False)
-            rospy.loginfo(armResponse)
-        except rospy.ServiceException as e:
-            print("Service call failed: %s" %e)
-            
-        return armResponse
-            
-            
-    def set_mode(self, mode):
-        rospy.wait_for_service(self.leader_data.header.name + '/mavros/set_mode', timeout=3)
-        try:
-            change_mode = rospy.ServiceProxy(self.leader_data.header.name + '/mavros/set_mode', SetMode)
-            response = change_mode(custom_mode=mode)
-            rospy.loginfo(response)
-        except rospy.ServiceException as e:
-            print("Set mode failed: %s" %e)
-            
-        return response
-            
-    def takeoff(self, altitude=5, latitude=0, longitude=0, min_pitch=0, yaw=0):
-        rospy.wait_for_service(self.leader_data.header.name + '/mavros/cmd/takeoff', timeout=3)
-        try:
-            takeoff_service = rospy.ServiceProxy(self.leader_data.header.name + '/mavros/cmd/takeoff', CommandTOL)
-            response = takeoff_service(altitude=altitude, latitude=latitude, longitude=longitude, min_pitch=min_pitch, yaw=yaw)
-            rospy.loginfo(response)
-        except rospy.ServiceException as e:
-            print("Takeoff failed: %s" %e)
-            
-        self.takeoff_altitude = altitude
-            
-        return response
-    
-    def goto_location(self, latitude, longitude, altitude, type_mask=4088, coordinate_frame=6):
-        
-        command = GlobalPositionTarget()
-        command.altitude = altitude
-        command.latitude = latitude
-        command.longitude = longitude
-        command.type_mask=type_mask
-        command.coordinate_frame=coordinate_frame
-        
-        self.leader_global_setpoint_publisher.publish(command)
-    
-    def check_takeoff_complete(self):
-        error = abs(self.takeoff_altitude - self.leader_data.local_position.z)
-        print("Takeoff error: ", error)
-        if error <= 0.2:
+        if followers_ready == self.n_followers:
             return True
         else:
             return False
-        
-    def check_land_complete(self):
-        if self.leader_data.local_position.z <= 0.5:
-            return True
-        else:
-            return False
-        
-    def check_target_location_reached(self, location):
-        if 
         
     def calculate_follower_coordinates(self):
         
@@ -199,76 +107,76 @@ class SwarmLeader():
     def leader_Heading_Subscriber_callback(self,mssg):
         self.leader_data.euler_orientation.yaw = mssg.data
         
-"""
-Functions to make it easy to convert between the different frames-of-reference. In particular these
-make it easy to navigate in terms of "metres from the current position" when using commands that take 
-absolute positions in decimal degrees.
+# """
+# Functions to make it easy to convert between the different frames-of-reference. In particular these
+# make it easy to navigate in terms of "metres from the current position" when using commands that take 
+# absolute positions in decimal degrees.
 
-The methods are approximations only, and may be less accurate over longer distances, and when close 
-to the Earth's poles.
+# The methods are approximations only, and may be less accurate over longer distances, and when close 
+# to the Earth's poles.
 
-Specifically, it provides:
-* get_location_metres - Get LocationGlobal (decimal degrees) at distance (m) North & East of a given LocationGlobal.
-* get_distance_metres - Get the distance between two LocationGlobal objects in metres
-* get_bearing - Get the bearing in degrees to a LocationGlobal
-"""
+# Specifically, it provides:
+# * get_location_metres - Get LocationGlobal (decimal degrees) at distance (m) North & East of a given LocationGlobal.
+# * get_distance_metres - Get the distance between two LocationGlobal objects in metres
+# * get_bearing - Get the bearing in degrees to a LocationGlobal
+# """
 
-def get_location_metres(original_location, dNorth, dEast):
-    """
-    Returns a LocationGlobal object containing the latitude/longitude `dNorth` and `dEast` metres from the 
-    specified `original_location`. The returned LocationGlobal has the same `alt` value
-    as `original_location`.
+# def get_location_metres(original_location, dNorth, dEast):
+#     """
+#     Returns a LocationGlobal object containing the latitude/longitude `dNorth` and `dEast` metres from the 
+#     specified `original_location`. The returned LocationGlobal has the same `alt` value
+#     as `original_location`.
 
-    The function is useful when you want to move the vehicle around specifying locations relative to 
-    the current vehicle position.
+#     The function is useful when you want to move the vehicle around specifying locations relative to 
+#     the current vehicle position.
 
-    The algorithm is relatively accurate over small distances (10m within 1km) except close to the poles.
+#     The algorithm is relatively accurate over small distances (10m within 1km) except close to the poles.
 
-    For more information see:
-    http://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
-    """
-    earth_radius = 6378137.0 #Radius of "spherical" earth
-    #Coordinate offsets in radians
-    dLat = dNorth/earth_radius
-    dLon = dEast/(earth_radius*math.cos(math.pi*original_location.lat/180))
+#     For more information see:
+#     http://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
+#     """
+#     earth_radius = 6378137.0 #Radius of "spherical" earth
+#     #Coordinate offsets in radians
+#     dLat = dNorth/earth_radius
+#     dLon = dEast/(earth_radius*math.cos(math.pi*original_location.lat/180))
 
-    #New position in decimal degrees
-    newlat = original_location.lat + (dLat * 180/math.pi)
-    newlon = original_location.lon + (dLon * 180/math.pi)
-    if type(original_location) is LocationGlobal:
-        targetlocation=LocationGlobal(newlat, newlon,original_location.alt)
-    elif type(original_location) is LocationGlobalRelative:
-        targetlocation=LocationGlobalRelative(newlat, newlon,original_location.alt)
-    else:
-        raise Exception("Invalid Location object passed")
+#     #New position in decimal degrees
+#     newlat = original_location.lat + (dLat * 180/math.pi)
+#     newlon = original_location.lon + (dLon * 180/math.pi)
+#     if type(original_location) is LocationGlobal:
+#         targetlocation=LocationGlobal(newlat, newlon,original_location.alt)
+#     elif type(original_location) is LocationGlobalRelative:
+#         targetlocation=LocationGlobalRelative(newlat, newlon,original_location.alt)
+#     else:
+#         raise Exception("Invalid Location object passed")
         
-    return targetlocation;
+#     return targetlocation;
 
 
-def get_distance_metres(aLocation1, aLocation2):
-    """
-    Returns the ground distance in metres between two LocationGlobal objects.
+# def get_distance_metres(aLocation1, aLocation2):
+#     """
+#     Returns the ground distance in metres between two LocationGlobal objects.
 
-    This method is an approximation, and will not be accurate over large distances and close to the 
-    earth's poles. It comes from the ArduPilot test code: 
-    https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
-    """
-    dlat = aLocation2.lat - aLocation1.lat
-    dlong = aLocation2.lon - aLocation1.lon
-    return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
+#     This method is an approximation, and will not be accurate over large distances and close to the 
+#     earth's poles. It comes from the ArduPilot test code: 
+#     https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
+#     """
+#     dlat = aLocation2.lat - aLocation1.lat
+#     dlong = aLocation2.lon - aLocation1.lon
+#     return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
 
 
-def get_bearing(aLocation1, aLocation2):
-    """
-    Returns the bearing between the two LocationGlobal objects passed as parameters.
+# def get_bearing(aLocation1, aLocation2):
+#     """
+#     Returns the bearing between the two LocationGlobal objects passed as parameters.
 
-    This method is an approximation, and may not be accurate over large distances and close to the 
-    earth's poles. It comes from the ArduPilot test code: 
-    https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
-    """	
-    off_x = aLocation2.lon - aLocation1.lon
-    off_y = aLocation2.lat - aLocation1.lat
-    bearing = 90.00 + math.atan2(-off_y, off_x) * 57.2957795
-    if bearing < 0:
-        bearing += 360.00
-    return bearing;
+#     This method is an approximation, and may not be accurate over large distances and close to the 
+#     earth's poles. It comes from the ArduPilot test code: 
+#     https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
+#     """	
+#     off_x = aLocation2.lon - aLocation1.lon
+#     off_y = aLocation2.lat - aLocation1.lat
+#     bearing = 90.00 + math.atan2(-off_y, off_x) * 57.2957795
+#     if bearing < 0:
+#         bearing += 360.00
+#     return bearing;
